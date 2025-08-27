ec942ec5feb7fc290741ef531f6ec524e0490bf1
hojatsadri <k.wolter33@gmail.com>
2025-08-27 20:16:41 +0330

    Added dynamic functionality to the agent

--- agent-template/src/lib.rs
+++ agent-template/src/lib.rs
@@ -13,11 +13,17 @@ struct Message {
 pub extern "C" fn run() {
         log("Hojjatsadri's agent has started!");
         //get the user input
+        // get the user input
         let input = utils::read_input();
-        //you can parse the input if you expect to receive a json
+        let input_str = String::from_utf8(input.clone()).unwrap_or_default();
+        let system_message;
+        if input_str.to_lowercase().contains("crypto") {
+        system_message = utils::system_message("You are a Crypto Expert. Answer all questions about blockchain and digital currencies.".to_string());
+        } else {
+        system_message = utils::system_message("You are a general helpful assistant named Atom.".to_string());
+        }
+        // you can parse the input if you expect to receive a json
         let messages = utils::parse_messages(&input);
-        //create a system message
-        let system_message = utils::system_message("Your name is Ding Dong Bot".to_string());
         //process the messages
         let modified_messages = utils::process_messages(system_message, messages);
 
@@ -35,8 +41,18 @@ pub extern "C" fn run() {
 
         let request = utils::prepare_request(&modified_messages_str);
         //call the service offchain
+        // call the service offchain and get the response
+ 
         let response = utils::call_ai_service(1, request);
-        //save the output
-        utils::save_output(&response);
-}
+
+        // 1. Convert the response to editable text
+        let response_str = String::from_utf8(response).unwrap();
+
+        // 2. Add our custom prefix to the response
+        let final_output = format!("Hojjat's Agent Answer: {}", response_str);
+
+        // 3. Save the final, modified output
+        utils::save_output(final_output.as_bytes());
+
+        
 
